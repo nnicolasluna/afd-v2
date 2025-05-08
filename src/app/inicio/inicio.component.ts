0475ea244f00a6a2589ab65a95bae87f059c3529
jluna <jluna@gambarte.com.bo>
2025-05-08 09:03:04 -0400

    en incio se quitaron los mapas otrs departamentos y se pintaron los municipios en leghtbar se agregaron los iconos per faltan algunos

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -7,37 +7,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss']
 })
-export class InicioComponent implements AfterViewInit {/* 
-  private map!: L.Map;
-  constructor(private http: HttpClient) { }
-  ngAfterViewInit(): void {
-    this.initMap();
-    this.cargarContornoBolivia();
-  }
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [-16.5, -64.15],
-      zoom: 6,
-      scrollWheelZoom: false,
-      dragging: false,
-      zoomControl: false,
-      doubleClickZoom: false
-    });
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-      maxZoom: 18
-    }).addTo(this.map);
-  }
-  private cargarContornoBolivia(): void {
-    this.http.get<any>('assets/municipios1.geo.json').subscribe(data => {
-      L.geoJSON(data, {
-        style: {
-          color: 'white',
-          weight: 2,
-          fillOpacity: 0
-        }
-      }).addTo(this.map);
-    });
-  } */
+export class InicioComponent implements AfterViewInit {
   private map!: L.Map;
 
   constructor(private http: HttpClient) { }
@@ -73,36 +43,65 @@ export class InicioComponent implements AfterViewInit {/*
         'Vinto',
         'Tiquipaya'
       ];
-      const featuresFiltradas = data.features.filter((feature: any) => {
-        const props = feature.properties;
-        return (
-          municipiosDeseados.includes(props.NOM_MUN)
-        );
+      const colores = [
+        '#FF5C5CB5',
+        '#F4A21ABD',
+        '#45818E',
+        '#8FCE00',
+        '#3521E8',
+        '#C7914A'
+      ];
+      const colorPorMunicipio = new Map<string, string>();
+      municipiosDeseados.forEach((nombre, index) => {
+        colorPorMunicipio.set(nombre, colores[index % colores.length]);
       });
 
+      const featuresFiltradas = data.features.filter((feature: any) => {
+        const props = feature.properties;
+        return municipiosDeseados.includes(props.NOM_MUN);
+      });
       const geojsonFiltrado = {
         ...data,
         features: featuresFiltradas
       };
 
       L.geoJSON(geojsonFiltrado, {
-        style: {
-          color: 'white',
-          weight: 2,
-          fillOpacity: 0
+        style: (feature: any) => {
+          const nombreMunicipio = feature.properties.NOM_MUN;
+          const color = colorPorMunicipio.get(nombreMunicipio) || 'white';
+          return {
+            color: '#FDE9A0',
+            weight: 2,
+            fillOpacity: 0.5,
+            fillColor: color
+          };
         }
       }).addTo(this.map);
     });
   }
-  private cargarContornoBolivia(): void {
-    this.http.get<any>('assets/bolivia.geo.json').subscribe(data => {
-      L.geoJSON(data, {
-        style: {
-          color: 'white',
-          weight: 2,
-          fillOpacity: 0
-        }
-      }).addTo(this.map);
-    });
-  }
+
+    private cargarContornoBolivia(): void {
+      this.http.get<any>('assets/bolivia.geo.json').subscribe(data => {
+        const departamentosDeseados = ['La Paz', 'Beni', 'Cochabamba'];
+    
+        const featuresFiltradas = data.features.filter((feature: any) => {
+          const props = feature.properties;
+          return departamentosDeseados.includes(props.NOM_DEP);
+        });
+    
+        const geojsonFiltrado = {
+          ...data,
+          features: featuresFiltradas
+        };
+    
+        L.geoJSON(geojsonFiltrado, {
+          style: {
+            color: 'gold',
+            weight: 2,
+            fillOpacity: 0
+          }
+        }).addTo(this.map);
+      });
+    }
+    
 }
