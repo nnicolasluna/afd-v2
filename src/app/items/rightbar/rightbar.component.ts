70c7f09d8410127290a9736d68cc01d4dda719bc
Joel Nicolas Luna <nnicolasluna@gmail.com>
2025-05-26 11:33:32 -0400

    integracion de gif en los mapas

--- src/app/items/rightbar/rightbar.component.ts
+++ src/app/items/rightbar/rightbar.component.ts
@@ -7,18 +7,18 @@ import { ModalStateService } from 'src/app/services/modal-state/modal-state.serv
   styleUrls: ['./rightbar.component.scss']
 })
 export class RightbarComponent {
-  @Input() mostrarRightBar :boolean = true
+  @Input() mostrarRightBar: boolean = true
   @Output() locationSelected = new EventEmitter<any>();
   rightBarToMenu: string = ''
-    constructor(private modalstateService: ModalStateService) { } // Inyecta el servicio
+  constructor(private modalstateService: ModalStateService) { } // Inyecta el servicio
 
   ciudades = [
-    { departamento: 'La Paz', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'San Buenaventura', color: '#FF5C5CB5', latMun: -14.45812, lonMun: -67.58674599999999 },
-    { departamento: 'La Paz', provincia: 'Abel Iturralde', AreaProt: 'PN ANMI Madidi', publoIndigena: 'Tacna', municipio: 'Palos Blancos', color: '#F4A21ABD', latMun: -15.583, lonMun: -67.25 },
-    { departamento: 'Beni', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'San Borja', color: '#45818E', latMun: -14.8583, lonMun: -66.7475 },
-    { departamento: 'Beni', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'Rurrenabaque', color: '#8FCE00', latMun: -14.442222, lonMun: -67.528333 },
-    { departamento: 'Cochabamba', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'Vinto', color: '#3521E8', latMun: -17.383333, lonMun: -66.3 },
-    { departamento: 'Cochabamba', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'Tiquipaya', color: '#C7914A', latMun: -17.333333, lonMun: -66.216667 }
+    { departamento: 'La Paz', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'San Buenaventura', png: 'assets/IconosMunicipios/SanBuenaventura.png', color: '#FF5C5CB5', latMun: -14.45812, lonMun: -67.58674599999999 },
+    { departamento: 'La Paz', provincia: 'Abel Iturralde', AreaProt: 'PN ANMI Madidi', publoIndigena: 'Tacna', municipio: 'Palos Blancos', png: 'assets/IconosMunicipios/Palos Blancos.png', color: '#F4A21ABD', latMun: -15.583, lonMun: -67.25 },
+    { departamento: 'Beni', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'San Borja', png: 'assets/IconosMunicipios/San Borja.png', color: '#45818E', latMun: -14.8583, lonMun: -66.7475 },
+    { departamento: 'Beni', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'Rurrenabaque', png: 'assets/IconosMunicipios/Rurrenabaque.png', color: '#8FCE00', latMun: -14.442222, lonMun: -67.528333 },
+    { departamento: 'Cochabamba', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'Vinto', png: 'assets/IconosMunicipios/Vinto.png', color: '#3521E8', latMun: -17.383333, lonMun: -66.3 },
+    { departamento: 'Cochabamba', provincia: 'Sur Yungas', AreaProt: 'RBTCO Pilon Lajas', publoIndigena: 'Moseten', municipio: 'Tiquipaya', png: 'assets/IconosMunicipios/Tiquipaya.png', color: '#C7914A', latMun: -17.333333, lonMun: -66.216667 }
   ];
 
   alternarRightBar() {
@@ -29,7 +29,7 @@ export class RightbarComponent {
   selectLocation(Municipio: string): void {
     const ciudadEncontrada = this.ciudades.find(ciudad => ciudad.municipio === Municipio);
     this.locationSelected.emit(ciudadEncontrada);
-    this.rightBarToMenu=Municipio
+    this.rightBarToMenu = Municipio
     this.modalstateService.mostrarCard();
     this.modalstateService.cerrarBtnEvaluamos();
   }
